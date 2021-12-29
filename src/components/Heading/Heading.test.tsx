0c21be2f6c4db3d04aefa5ccff150dc829a03333
jefferson1104 <jefferson1104junior@gmail.com>
2021-12-29 18:09:20 -0300

    adjustes in the Heading component

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -31,7 +31,7 @@ describe('Heading component', () => {
     const heading = screen.getByRole('heading', { name: /won games/i })
 
     expect(heading).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'border-left': '0.7rem solid #F231A5'
     })
   })
 
@@ -41,6 +41,45 @@ describe('Heading component', () => {
     const heading = screen.getByRole('heading', { name: /won games/i })
 
     expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
+  })
+
+  it('should render a heading with a small size', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
+
+    const heading = screen.getByRole('heading', { name: /won games/i })
+
+    expect(heading).toHaveStyle({ 'font-size': '1.6rem' })
+    expect(heading).toHaveStyleRule('width', '3rem', { modifier: '::after' })
+  })
+
+  it('should render a heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="primary" lineLeft lineBottom>
+        Won Games
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /won games/i })
+
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
+  })
+
+  it('should render a heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="secondary" lineLeft lineBottom>
+        Won Games
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /won games/i })
+
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
       modifier: '::after'
     })
   })
