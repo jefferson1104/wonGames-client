edb0f93efe4850f2950840dd1d471fc02f767523
jefferson1104 <jefferson1104junior@gmail.com>
2022-01-03 01:00:20 -0300

    create Highlight component

---
+++ src/components/Highlight/Highlight.stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Red Dead it`s back',
+    subtitle: 'Come see Jhon`s new adventures',
+    backgroundImage: '/img/red-dead-img.jpg',
+    buttonLabel: 'Buy now',
+    buttonLink: '/highlight'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
---
+++ src/components/Highlight/Highlight.test.tsx
@@ -0,0 +1,71 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Highlight from '.'
+import * as S from './styles'
+
+const props = {
+  title: 'Heading 1',
+  subtitle: 'Heading 2',
+  backgroundImage: '/img/red-dead-img.jpg',
+  buttonLabel: 'Buy now',
+  buttonLink: '/highlight'
+}
+
+describe('Highlight component', () => {
+  it('should render headings and button', () => {
+    renderWithTheme(<Highlight {...props} />)
+
+    const heading1 = screen.getByRole('heading', { name: /heading 1/i })
+    const heading2 = screen.getByRole('heading', { name: /heading 2/i })
+    const link = screen.getByRole('link', { name: /buy now/i })
+
+    expect(heading1).toBeInTheDocument()
+    expect(heading2).toBeInTheDocument()
+    expect(link).toBeInTheDocument()
+  })
+
+  it('should render background image', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyle({
+      backgroundImage: `url(${props.backgroundImage})`
+    })
+  })
+
+  it('should render float image', () => {
+    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />)
+
+    const floatImg = screen.getByRole('img', { name: props.title })
+
+    expect(floatImg).toHaveAttribute('src', '/float-image.png')
+  })
+
+  it('should render align right by default', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatimage content'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it('should render align left', () => {
+    const { container } = renderWithTheme(
+      <Highlight {...props} alignment="left" />
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatimage'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'left', {
+      modifier: `${S.Content}`
+    })
+  })
+})
