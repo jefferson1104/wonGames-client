dc12b2d82d3d6c89f0d8b5d3f5cf2ee0f20cb291
jefferson1104 <jefferson1104junior@gmail.com>
2022-01-13 10:37:34 -0300

    create showcase component

---
+++ src/components/Showcase/index.tsx
@@ -0,0 +1,29 @@
+import Heading from 'components/Heading'
+import Highlight, { HighlightProps } from 'components/Highlight'
+import GameCardSlider from 'components/GameCardSlider'
+import { GameCardProps } from 'components/GameCard'
+
+import * as S from './styles'
+
+export type ShowcaseProps = {
+  title?: string
+  highlight?: HighlightProps
+  games?: GameCardProps[]
+}
+
+const Showcase = ({ title, highlight, games }: ShowcaseProps) => {
+  return (
+    <S.Wrapper>
+      {!!title && (
+        <Heading lineLeft lineColor="secondary">
+          {title}
+        </Heading>
+      )}
+
+      {!!highlight && <Highlight {...highlight} />}
+      {!!games && <GameCardSlider items={games} />}
+    </S.Wrapper>
+  )
+}
+
+export default Showcase
