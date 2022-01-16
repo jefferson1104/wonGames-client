7811231b9614b1c2f0a1e8d18590aeb1f204a1f4
jefferson1104 <jefferson1104junior@gmail.com>
2022-01-16 20:07:53 -0300

    create ProfileMenu component

---
+++ src/components/ProfileMenu/ProfileMenu.stories.tsx
@@ -0,0 +1,24 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import ProfileMenu, { ProfileMenuProps } from '.'
+
+export default {
+  title: 'ProfileMenu',
+  component: ProfileMenu,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: {
+    activeLink: {
+      control: {
+        type: 'inline-check',
+        options: ['/profile/me', '/profile/cards', '/profile/orders']
+      }
+    }
+  }
+} as Meta
+
+export const Default: Story<ProfileMenuProps> = (args) => (
+  <ProfileMenu {...args} />
+)
