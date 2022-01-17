c5d681e2fc01727180dc9944b5f8bad008012b39
jefferson1104 <jefferson1104junior@gmail.com>
2022-01-16 23:22:28 -0300

    Create OrdersList component

---
+++ src/components/OrdersList/index.tsx
@@ -0,0 +1,31 @@
+import Heading from 'components/Heading'
+import GameItem, { GameItemProps } from 'components/GameItem'
+
+import * as S from './styles'
+import Empty from 'components/Empty'
+
+export type OrdersListProps = {
+  items?: GameItemProps[]
+}
+
+const OrdersList = ({ items = [] }: OrdersListProps) => {
+  return (
+    <S.Wrapper>
+      <Heading lineBottom lineColor="primary" color="black" size="small">
+        My orders
+      </Heading>
+
+      {items.length ? (
+        items?.map((item) => <GameItem key={item.downloadLink} {...item} />)
+      ) : (
+        <Empty
+          title="You have no orders yet"
+          description="Go back to the store and explore great games and offers"
+          hasLink
+        />
+      )}
+    </S.Wrapper>
+  )
+}
+
+export default OrdersList
