e7f18353d06fd271c4d8e67e66734ef50c950ddf
jefferson1104 <jefferson1104junior@gmail.com>
2022-01-26 00:36:30 -0300

    create useCart hook structure, CartContext and hook tests

---
+++ src/hooks/use-cart/index.tsx
@@ -0,0 +1,69 @@
+import { useQueryGames } from 'graphql/queries/games'
+import { createContext, useContext, useEffect, useState } from 'react'
+import FormatPrice from 'utils/format-price'
+import { getStorageItem } from 'utils/localStorage'
+
+const CART_KEY = 'cartItems'
+
+type CartItem = {
+  id: string
+  img: string
+  title: string
+  price: string
+}
+
+export type CartContextData = {
+  items: CartItem[]
+}
+
+export const CartContextDefaultValues = {
+  items: []
+}
+
+export const CartContext = createContext<CartContextData>(
+  CartContextDefaultValues
+)
+
+export type CartProviderProps = {
+  children: React.ReactNode
+}
+
+const CartProvider = ({ children }: CartProviderProps) => {
+  const [cartItems, setCartItems] = useState<string[]>([])
+
+  useEffect(() => {
+    const data = getStorageItem(CART_KEY)
+
+    if (data) {
+      setCartItems(data)
+    }
+  }, [])
+
+  const { data } = useQueryGames({
+    skip: !cartItems?.length,
+    variables: {
+      where: {
+        id: cartItems
+      }
+    }
+  })
+
+  return (
+    <CartContext.Provider
+      value={{
+        items: data?.games.map((game) => ({
+          id: game.id,
+          img: `http://localhost:1337${game.cover?.url}`,
+          price: FormatPrice(game.price),
+          title: game.name
+        }))
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+const useCart = () => useContext(CartContext)
+
+export { CartProvider, useCart }
