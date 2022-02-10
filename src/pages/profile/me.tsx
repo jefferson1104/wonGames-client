fa31266ea034ac192ef84174a78e55a0607feaa8
jefferson1104 <jefferson1104junior@gmail.com>
2022-02-10 20:12:48 -0300

    configure authenticated fetch in apollo HTTP and query getProfileMe

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -1,21 +1,31 @@
 import { GetServerSidePropsContext } from 'next'
+
+import { QueryProfileMe } from 'graphql/generated/QueryProfileMe'
+import { QUERY_PROFILE_ME } from 'graphql/queries/profile'
+
 import protectedRoutes from 'utils/protected-routes'
+import { initializeApollo } from 'utils/apollo'
 
 import Profile from 'templates/Profile'
-import FormProfile from 'components/FormProfile'
+import FormProfile, { FormProfileProps } from 'components/FormProfile'
 
-export default function Me() {
+export default function Me(props: FormProfileProps) {
   return (
     <Profile>
-      <FormProfile />
+      <FormProfile {...props} />
     </Profile>
   )
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
+
+  const { data } = await apolloClient.query<QueryProfileMe>({
+    query: QUERY_PROFILE_ME
+  })
 
   return {
-    props: { session }
+    props: { session, username: data.me?.username, email: data.me?.email }
   }
 }
