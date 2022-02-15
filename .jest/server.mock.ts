dce3daca36d39e6e73993898017830a6399abe97
jefferson1104 <jefferson1104junior@gmail.com>
2022-02-15 01:14:29 -0300

    install and configure Mock Service Worker (MSW)

---
+++ .jest/server.mock.ts
@@ -0,0 +1,18 @@
+global.fetch = require('node-fetch')
+
+import { server } from '../src/utils/mockServer/server'
+
+beforeAll(() => {
+  // colocar para ficar escutando todas as chamadas nos testes
+  server.listen()
+})
+
+afterEach(() => {
+  // apos todos os testes resetar handlers para caso eles sejam chamados novamente
+  server.restoreHandlers()
+})
+
+afterAll(() => {
+  // fechar o server e limpar os testes
+  server.close()
+})
--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,2 +1,9 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+
+// colocando para carregar as variaveis de ambiente nos testes
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: '.env.development'
+})
