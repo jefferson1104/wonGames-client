e229a5e11975a5a41a206b574c244076e7109272
jefferson1104 <jefferson1104junior@gmail.com>
2022-02-15 20:53:23 -0300

    create handler of MSW for the tests in ForgotPassword

---
+++ src/components/FormForgotPassword/FormForgotPassword.test.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import 'server.mock'
+import userEvent from '@testing-library/user-event'
+import { render, screen } from 'utils/test-utils'
+
+import FormForgotPassword from '.'
+
+// mock useRouter
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+let query = {}
+useRouter.mockImplementation(() => ({
+  query
+}))
+
+describe('<FormForgotPassword />', () => {
+  it('should render the form', () => {
+    render(<FormForgotPassword />)
+
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /send email/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should validate the email', async () => {
+    render(<FormForgotPassword />)
+
+    await userEvent.type(
+      screen.getByPlaceholderText(/email/i),
+      'valid@email.com'
+    )
+
+    userEvent.click(screen.getByRole('button', { name: /send email/i }))
+
+    expect(
+      await screen.findByText(/you just received an email!/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should show an invalid email', async () => {
+    render(<FormForgotPassword />)
+
+    await userEvent.type(screen.getByPlaceholderText(/email/i), 'invalid-email')
+
+    userEvent.click(screen.getByRole('button', { name: /send email/i }))
+
+    expect(
+      await screen.findByText(/must be a valid email/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should show an inexistent email error', async () => {
+    render(<FormForgotPassword />)
+
+    await userEvent.type(
+      screen.getByPlaceholderText(/email/i),
+      'false@email.com'
+    )
+
+    userEvent.click(screen.getByRole('button', { name: /send email/i }))
+
+    expect(
+      await screen.findByText(/This email does not exist/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should autofill if comes via logged user', async () => {
+    query = { email: 'valid@email.com' }
+    render(<FormForgotPassword />)
+
+    expect(screen.getByPlaceholderText(/email/i)).toHaveValue('valid@email.com')
+  })
+})
