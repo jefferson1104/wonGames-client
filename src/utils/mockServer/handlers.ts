b353af396367da4104c482d0ec42d0ac100e97f2
jefferson1104 <jefferson1104junior@gmail.com>
2022-02-15 23:22:57 -0300

    create handler of MSW for the tests in ResetPassword

--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -4,6 +4,12 @@ type LoginReqBody = {
   email: string
 }
 
+type ResetReqBody = {
+  code: string
+  password: string
+  passwordConfirmation: string
+}
+
 // onde interceptamos as chamadas
 export const handlers = [
   rest.post<LoginReqBody>(
@@ -35,6 +41,42 @@ export const handlers = [
         ctx.status(200),
         ctx.json({
           ok: true
+        })
+      )
+    }
+  ),
+
+  rest.post<ResetReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+    (req, res, ctx) => {
+      const { code } = req.body
+
+      // se houver erro
+      if (code === 'wrong_code') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [
+                  {
+                    message: 'Incorrect code provided.'
+                  }
+                ]
+              }
+            ]
+          })
+        )
+      }
+
+      // se houver sucesso
+      return res(
+        ctx.status(200),
+        ctx.json({
+          user: {
+            email: 'valid@email.com'
+          }
         })
       )
     }
