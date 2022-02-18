8b212ae809ee39e74e31fe14ad6198412172cea4
jefferson1104 <jefferson1104junior@gmail.com>
2022-02-17 23:41:59 -0300

    create query wishlist and hook useQueryWishlist

---
+++ src/graphql/queries/wishlist.ts
@@ -0,0 +1,28 @@
+import { gql, useQuery, QueryHookOptions } from '@apollo/client'
+import { GameFragment } from 'graphql/fragments/game'
+import {
+  QueryWishlist,
+  QueryWishlistVariables
+} from 'graphql/generated/QueryWishlist'
+
+export const QUERY_WISHLIST = gql`
+  query QueryWishlist($identifier: String!) {
+    wishlists(where: { user: { email: $identifier } }) {
+      id
+      games {
+        ...GameFragment
+      }
+    }
+  }
+  ${GameFragment}
+`
+
+// hook para utilizar no client side
+export function useQueryWishlist(
+  options?: QueryHookOptions<QueryWishlist, QueryWishlistVariables>
+) {
+  return useQuery<QueryWishlist, QueryWishlistVariables>(
+    QUERY_WISHLIST,
+    options
+  )
+}
