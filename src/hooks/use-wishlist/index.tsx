e8a96ae4f7f48d353392d1cc9e44ada1648bfe66
jefferson1104 <jefferson1104junior@gmail.com>
2022-02-20 10:57:01 -0300

    method to create or update wishlish

--- src/hooks/use-wishlist/index.tsx
+++ src/hooks/use-wishlist/index.tsx
@@ -1,9 +1,14 @@
-import { createContext, useContext, useEffect, useState } from 'react'
-import { GameCardProps } from 'components/GameCard'
-import { useQueryWishlist } from 'graphql/queries/wishlist'
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
 import { useSession } from 'next-auth/client'
-import { gamesMapper } from 'utils/mappers'
+import { useMutation } from '@apollo/client'
+import { useQueryWishlist } from 'graphql/queries/wishlist'
 import { QueryWishlist_wishlists_games } from 'graphql/generated/QueryWishlist'
+import {
+  MUTATION_CREATE_WISHLIST,
+  MUTATION_UPDATE_WISHLIST
+} from 'graphql/mutations/wishlist'
+import { gamesMapper } from 'utils/mappers'
+import { GameCardProps } from 'components/GameCard'
 
 // criando tipos de dados do contexto da wishlist
 export type WishlistContextData = {
@@ -36,9 +41,33 @@ export type WishlistProviderProps = {
 // criando o provider
 const WishlistProvider = ({ children }: WishlistProviderProps) => {
   const [session] = useSession()
+  const [wishlistId, setWishlistId] = useState<string | null>()
   const [wishlistItems, setWishlistItems] = useState<
     QueryWishlist_wishlists_games[]
   >([])
+
+  // Mutation GRAPHQL para criar wishlist
+  const [createList, { loading: loadingCreate }] = useMutation(
+    MUTATION_CREATE_WISHLIST,
+    {
+      context: { session },
+      onCompleted: (data) => {
+        setWishlistItems(data?.createWishlist?.wishlist?.games || [])
+        setWishlistId(data?.createWishlist?.wishlist?.id)
+      }
+    }
+  )
+
+  // Mutation GRAPHQL para atualizar a wishlist
+  const [updateList, { loading: loadingUpdate }] = useMutation(
+    MUTATION_UPDATE_WISHLIST,
+    {
+      context: { session },
+      onCompleted: (data) => {
+        setWishlistItems(data?.updateWishlist?.wishlist?.games || [])
+      }
+    }
+  )
 
   // buscando games da wishlist, somente se houver uma session
   const { data, loading } = useQueryWishlist({
@@ -52,14 +81,38 @@ const WishlistProvider = ({ children }: WishlistProviderProps) => {
   // useEffect para gravar os dados retornados da query em um state
   useEffect(() => {
     setWishlistItems(data?.wishlists[0]?.games || [])
+    setWishlistId(data?.wishlists[0]?.id)
   }, [data])
+
+  // wishlistItems atualizou? este metodo é chamado e retorna o id dos games
+  const wishListIds = useMemo(
+    () => wishlistItems.map((game) => game.id),
+    [wishlistItems]
+  )
 
   // verifica se o id do jogo esta na wishlist
   const isInWishlist = (id: string) =>
     !!wishlistItems.find((game) => game.id === id)
 
-  // adiciona o jogo na wishlist
-  const addToWishlist = (id: string) => {}
+  // adiciona o jogo na wishlist ou atualiza a wishlist
+  const addToWishlist = (id: string) => {
+    // se nao existir a wishlist entao cria-se
+    if (!wishlistId) {
+      return createList({
+        variables: { input: { data: { games: [...wishListIds, id] } } }
+      })
+    }
+
+    // se ja existe a wishlist ele atualiza
+    return updateList({
+      variables: {
+        input: {
+          where: { id: wishlistId },
+          data: { games: [...wishListIds, id] }
+        }
+      }
+    })
+  }
 
   // remove o jogo da wishlist
   const removeFromWishlist = (id: string) => {}
--- src/hooks/use-wishlist/useWishlist.test.tsx
+++ src/hooks/use-wishlist/useWishlist.test.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { MockedProvider } from '@apollo/client/testing'
 import { renderHook } from '@testing-library/react-hooks'
+import { act, waitFor } from 'utils/test-utils'
 
 import { useWishlist, WishlistProvider } from '.'
-import { wishlistItems, wishlistMock } from './mock'
+import {
+  createWishlistMock,
+  updateWishlistMock,
+  wishlistItems,
+  wishlistMock
+} from './mock'
 
 // criando um mock da session do next-auth/client
 const useSession = jest.spyOn(require('next-auth/client'), 'useSession')
@@ -19,7 +25,7 @@ describe('useWishlist', () => {
       </MockedProvider>
     )
 
-    // getting result from 'Hook'
+    // Render hook
     const { result, waitForNextUpdate } = renderHook(() => useWishlist(), {
       wrapper
     })
@@ -52,5 +58,49 @@ describe('useWishlist', () => {
     expect(result.current.isInWishlist('1')).toBe(true)
     expect(result.current.isInWishlist('2')).toBe(true)
     expect(result.current.isInWishlist('3')).toBe(false)
+  })
+
+  it('should add item in wishlist creating a new list', async () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <MockedProvider mocks={[createWishlistMock]}>
+        <WishlistProvider>{children}</WishlistProvider>
+      </MockedProvider>
+    )
+
+    const { result, waitForNextUpdate } = renderHook(() => useWishlist(), {
+      wrapper
+    })
+
+    // utilize o act do testing-library para rodar coisas que modificam um estado
+    act(() => {
+      result.current.addToWishlist('3')
+    })
+
+    await waitForNextUpdate()
+    expect(result.current.items).toStrictEqual([wishlistItems[2]])
+  })
+
+  it('should add item in wishlist updating the current list', async () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <MockedProvider mocks={[wishlistMock, updateWishlistMock]}>
+        <WishlistProvider>{children}</WishlistProvider>
+      </MockedProvider>
+    )
+
+    const { result, waitForNextUpdate } = renderHook(() => useWishlist(), {
+      wrapper
+    })
+
+    // wait for the data to load
+    await waitForNextUpdate()
+
+    // utilize o act do testing-library para rodar coisas que modificam um estado
+    act(() => {
+      result.current.addToWishlist('3')
+    })
+
+    await waitFor(() => {
+      expect(result.current.items).toStrictEqual(wishlistItems)
+    })
   })
 })
