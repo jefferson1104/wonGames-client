6dc53ac5a854adf4a8e2206586c14a4268c9a825
jefferson1104 <jefferson1104junior@gmail.com>
2022-02-22 18:28:06 -0300

    refact and remove unusable files

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -7,9 +7,6 @@ import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 
 import Cart, { CartProps } from 'templates/Cart'
 
-import itemsMock from 'components/CartList/mock'
-import cardsMock from 'components/PaymentOptions/mock'
-
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
@@ -25,9 +22,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
       session,
-      items: itemsMock,
-      total: '$ 430,00',
-      cards: cardsMock,
       recommendedTitle: data.recommended?.section?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedHighlight: highlightMapper(
--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -7,25 +7,22 @@ import { HighlightProps } from 'components/Highlight'
 import Heading from 'components/Heading'
 import Showcase from 'components/Showcase'
 import CartList, { CartListProps } from 'components/CartList'
-import PaymentOptions, { PaymentOptionsProps } from 'components/PaymentOptions'
+import PaymentForm from 'components/PaymentForm'
 
+import { Info } from '@styled-icons/material-outlined/Info'
 import * as S from './styles'
 
 export type CartProps = {
   recommendedTitle: string
   recommendedGames: GameCardProps[]
   recommendedHighlight: HighlightProps
-} & CartListProps &
-  Pick<PaymentOptionsProps, 'cards'>
+} & CartListProps
 
 const Cart = ({
   recommendedTitle,
   recommendedGames,
-  recommendedHighlight,
-  cards
+  recommendedHighlight
 }: CartProps) => {
-  const handlePayment = () => ({})
-
   return (
     <Base>
       <Container>
@@ -34,8 +31,16 @@ const Cart = ({
         </Heading>
         <S.Content>
           <CartList />
-          <PaymentOptions cards={cards} handlePayment={handlePayment} />
+          <PaymentForm />
         </S.Content>
+        <S.Text>
+          <Info size={18} /> Your purchase is protected by a secure connection
+          from the WON platform. By purchasing from our store you agree and
+          agree to our <a href="#">terms of use.</a> After making the purchase
+          you are entitled to a refund within a maximum of 30 days, without any
+          additional cost, as long as the download of the purchased game has not
+          occurred after your purchase.
+        </S.Text>
         <Divider />
       </Container>
 
