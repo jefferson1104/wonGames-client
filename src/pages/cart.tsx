4f44c3dcd949333049ddcf618d334334456006b8
jefferson1104 <jefferson1104junior@gmail.com>
2022-02-22 18:11:38 -0300

    protecting the /cart route for logged in users only

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,5 +1,7 @@
+import { GetServerSidePropsContext } from 'next'
 import { initializeApollo } from 'utils/apollo'
 import { gamesMapper, highlightMapper } from 'utils/mappers'
+import protectedRoutes from 'utils/protected-routes'
 import { QueryRecommended } from 'graphql/generated/QueryRecommended'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 
@@ -12,8 +14,9 @@ export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
 
   const { data } = await apolloClient.query<QueryRecommended>({
     query: QUERY_RECOMMENDED
@@ -21,6 +24,7 @@ export async function getServerSideProps() {
 
   return {
     props: {
+      session,
       items: itemsMock,
       total: '$ 430,00',
       cards: cardsMock,
