5f4e96e540b2e16e0837e3c4e4e744877d1ba087
jefferson1104 <jefferson1104junior@gmail.com>
2022-02-25 00:03:41 -0300

    catching errors and presenting in stripe CardElement

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,19 +1,37 @@
+import { useState } from 'react'
+
+import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
 import { CardElement } from '@stripe/react-stripe-js'
+
 import Button from 'components/Button'
 import Heading from 'components/Heading'
 
-import { ShoppingCart } from 'styled-icons/material-outlined'
-
+import { ErrorOutline, ShoppingCart } from 'styled-icons/material-outlined'
 import * as S from './styles'
 
 const PaymentForm = () => {
+  const [error, setError] = useState<string | null>(null)
+
+  const handleChange = async (event: StripeCardElementChangeEvent) => {
+    setError(event.error ? event.error.message : '')
+  }
+
   return (
     <S.Wrapper>
       <S.Body>
         <Heading color="black" lineBottom size="small">
           Payment
         </Heading>
-        <CardElement options={{ hidePostalCode: true }} />
+        <CardElement
+          options={{ hidePostalCode: true }}
+          onChange={handleChange}
+        />
+        {error && (
+          <S.Error>
+            <ErrorOutline size={20} />
+            {error}
+          </S.Error>
+        )}
       </S.Body>
       <S.Footer>
         <Button as="a" fullWidth minimal>
