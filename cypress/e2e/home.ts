c159ddb43e8306050b8ed73e2b0fb169c4440f30
jefferson1104 <jefferson1104junior@gmail.com>
2022-03-14 23:44:48 -0300

    Using scope with within() method of the cypress

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -2,6 +2,13 @@
 
 describe('Home Page', () => {
   it('Should render home sections', () => {
+    // visitar a home page
     cy.visit('/')
+
+    // pegando o slider e o conteudo dentro dele com o metodo within()
+    cy.get('.slick-slider').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i })
+      cy.findByRole('link', { name: /buy now/i })
+    })
   })
 })
