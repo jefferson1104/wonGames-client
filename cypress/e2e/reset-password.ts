42baff1910a3437111745208afbfc82034393a16
jefferson1104 <jefferson1104junior@gmail.com>
2022-03-22 18:42:14 -0300

    reset-password e2e test, happy path

--- cypress/e2e/reset-password.ts
+++ cypress/e2e/reset-password.ts
@@ -38,4 +38,40 @@ describe('Reset Password', () => {
 
     cy.findByText(/Incorrect code provided/i).should('exist')
   })
+
+  it('Should fill the input and redirect to the home page with the user signed in', () => {
+    // interceptando a rota do backend
+    cy.intercept('POST', '**/auth/reset-password', {
+      statusCode: 200,
+      body: { user: { email: 'e2e@wongames.com' }}
+    })
+
+    // interceptando a rota do callback de credentials do next-auth
+    cy.intercept('POST', '**/auth/callback/credentials*', {
+      statusCode: 200,
+      body: { user: { email: 'e2e@wongames.com' }}
+    })
+
+    // interceptando a rota de session do next-auth
+    cy.intercept('GET', '**/auth/session*', {
+      statusCode: 200,
+      body: { user: { name: 'e2e_user', email: 'e2e@wongames.com' }}
+    })
+
+    // usuario acessa a pagina de reset
+    cy.visit('/reset-password?code=valid_token')
+
+
+    // preencher as senhas (já com o token valido) e clicar para realizar login
+    cy.findAllByPlaceholderText(/^password/i).type('123456')
+    cy.findAllByPlaceholderText(/confirm password/i).type('123456')
+    cy.findByRole('button', { name: /reset password/i }).click()
+
+    // redireciona para a home
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+    // tem o usuario logado com name no menu
+    cy.findByText(/e2e_user/i).should('exist')
+
+  })
 })
