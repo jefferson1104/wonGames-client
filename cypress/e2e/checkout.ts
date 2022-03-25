ff0b0e48d62ea99aa7bf34b8fb5e0d35ab7a7bfc
jefferson1104 <jefferson1104junior@gmail.com>
2022-03-25 00:46:03 -0300

    checkout e2e tests, paid games testing

--- cypress/e2e/checkout.ts
+++ cypress/e2e/checkout.ts
@@ -4,7 +4,7 @@ import { createUser } from "../support/generate"
 
 describe('Checkout', () => {
   let user: User
-  describe('Free Games', () => {
+  describe.skip('Free Games', () => {
     before(() => {
       user = createUser()
     })
@@ -60,7 +60,62 @@ describe('Checkout', () => {
 
 
   describe('Paid Games', () => {
+    before(() => {
+      user = createUser()
+    })
 
+    it('Should buy paid games', () => {
+      // acessa pagina de sign-up criar um usuario e verifica se foi para a home
+      cy.visit('/sign-up')
+      cy.signUp(user)
+      cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+      // ir para explore page e verificar se esta na rota /games
+      cy.findByRole('link', { name: /explore/i }).click()
+      cy.url().should('eq', `${Cypress.config().baseUrl}/games`)
+
+      // filtrar por jogos mais caros e verificar se na url esta o parametro correto
+      cy.findByText('Highest to lowest').click()
+      cy.url().should('contain', 'sort=price%3Adesc')
+
+      // adicionar um jogo no carrinho
+      cy.addToCartByIndex(0)
+
+      // verificar se tem algum jogo no carrinho e abrir dropdown
+      cy.findAllByLabelText(/cart items/i).first().should('have.text', 1).click()
+
+      // clicar no button para fazer a compra
+      cy.getByDataCy('cart-list').within(() => {
+        cy.findByText(/buy it now/i).click()
+      })
+
+      // botao de comprar deve estar desabilitado
+      cy.findByRole('button', { name: /buy now/i }).should('have.attr', 'disabled')
+
+      // preencher os campos com os dados do cartão
+      cy.fillElementsInput('cardNumber', '4242424242424242')
+      cy.fillElementsInput('cardExpiry', '1030')
+      cy.fillElementsInput('cardCvc', '020')
+
+      // clicar no botão para comprar
+      cy.findByRole('button', { name: /buy now/i }).click()
+
+      // verificar se foi redirecionado para pagina de sucesso
+      cy.url().should('eq', `${Cypress.config().baseUrl}/success`)
+
+      // verificar o texto da pagina de sucesso
+      cy.findByRole('heading', { name: /Your purchase was successful!/i }).should('exist')
+    })
+
+    it('Should show games in order page', () => {
+      cy.visit('/profile/orders')
+      cy.location('href').should('eq', `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/orders`)
+
+      cy.signIn(user.email, user.password)
+      cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/orders`)
+
+      cy.getByDataCy('game-item').should('have.length', 1)
+    })
   })
 })
 
